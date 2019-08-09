cce9467ede766a99ac7f71cf075346746e20f457
jun68ykt <jun68ykt@teratail>
2019-08-09 22:00:19 +0900

    result4 の作成部分を修正(その2)

--- main.js
+++ main.js
@@ -20,27 +20,10 @@ document.addEventListener('DOMContentLoaded', function() {
 
       var result4 = result3.map(ary =>  ary.map(e => [e]));
 
-      for (var i = 0; i<result4.length; i++) {
-        if(result4[i][4][0].indexOf("x") >= 0){
-          result4[i][0].unshift(1000);
-          result4[i][0].unshift("x");
-          result4[i][1].unshift(100);
-          result4[i][1].unshift("x");
-          result4[i][2].unshift(10);
-          result4[i][2].unshift("x");
-          result4[i][3].unshift(1);
-          result4[i][3].unshift("x");
-        } else if (result4[i][4][0].indexOf("y") >= 0){
-          result4[i][0].unshift(1000);
-          result4[i][0].unshift("y");
-          result4[i][1].unshift(100);
-          result4[i][1].unshift("y");
-          result4[i][2].unshift(10);
-          result4[i][2].unshift("y");
-          result4[i][3].unshift(1);
-          result4[i][3].unshift("y");
-        }
-      }
+      result4 = result4.map(ary => ary.map(
+                  (a, i) => (i <= 3 ? [ary[4][0], 10**(3-i), a[0]] : a)
+                ));
+
       console.log(result4);
 
       var result5 = [];
